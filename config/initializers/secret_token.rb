32e43017595b64c3a0b2a0e4044b6463766291e3
Owen Pragel <owenpragel@gmail.com>
2014-02-13 11:29:50 -0800

    Commit Changes for Assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CPSC492Test::Application.config.secret_key_base = 'bf16adb77a224f1d34856d8eb00065aa48c0376b2af5fffba8e5eb352cb20d4ed3a30548a78ba1d273ad2c81559b04a4124615a6f5c5e85acdddf8351a868df2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CPSC492Test::Application.config.session_store :cookie_store, key: '_CPSC492-Test_session'
